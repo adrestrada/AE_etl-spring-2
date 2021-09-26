8351e10edfd70fb978dc04ce91068988f6a323ef
ADRIANA ESTRADA.‿✿• <adr.estrada@gmail.com>
2021-09-25 20:57:23 -0400

    Update HiveClient.scala

--- HiveClient.scala
+++ HiveClient.scala
@@ -14,12 +14,3 @@ trait HiveClient {
   val stmt: Statement = connection.createStatement()
 }
 
-//val driverName: String = "org.apache.hive.jdbc.HiveDriver"
-//  Class.forName(driverName)
-//  val connectionString: String =
-//  "jdbc:hive2://quickstart.cloudera:10000/bdsf2001_adriest;user=adriest;password=valeria9"
-//  val  connection = DriverManager.getConnection(connectionString)
-//  val stmt = connection.createStatement()
-//  stmt.close()
-//  connection.close()
-//}
